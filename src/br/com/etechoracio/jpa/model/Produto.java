e44d3c283e9dbaebb57f99091fb4634bbacbbfb5
danielle-santos <danielle.danyy92santos@gmail.com>
2019-03-15 22:42:34 -0300

    [Ex.12] - Atualização do Relacionamento ManyToOne

--- src/br/com/etechoracio/jpa/model/Produto.java
+++ src/br/com/etechoracio/jpa/model/Produto.java
@@ -2,6 +2,7 @@ package br.com.etechoracio.jpa.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -28,7 +29,7 @@ public class Produto {
 	@Column(name = "NR_PRECO")
 	private double preco;
 
-	@ManyToOne
+	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name = "ID_CATEGORIA")
 	private Categoria categoria;
 }
